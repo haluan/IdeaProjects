1cdef897a56e663292829f4f84213e826dcafce5
Haluan <haluan@icehousecorp.com>
2016-03-07 13:09:04 +0700

    add future thread inside thread

--- Lab04/src/Main.java
+++ Lab04/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         final int SIKLUS = 1;
         int initAyam = 9;
         int targetAyam = 900;
-        Future<Integer> result = ovipar(initAyam, targetAyam, SIKLUS);
+        Future<Integer> result = oviparCounter(initAyam, targetAyam, SIKLUS);
         try {
             System.out.println(targetAyam + " ayam dalam "+ result.get() + " siklus");
         } catch (InterruptedException e) {
@@ -24,7 +24,7 @@ public class Main {
         }
         int initKucing = 23;
         int targetKucing = 830;
-        result = vivipar(initKucing, targetKucing, SIKLUS);
+        result = viviparCounter(initKucing, targetKucing, SIKLUS);
         try {
             System.out.println(targetKucing + " kucing dalam "+ result.get() +" siklus");
         } catch (InterruptedException e) {
@@ -32,7 +32,7 @@ public class Main {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        result = ovivipar(4, 900, SIKLUS);
+        result = oviviparCounter(4, 900, SIKLUS);
         try {
             System.out.println(result.get());
         } catch (InterruptedException e) {
@@ -41,58 +41,86 @@ public class Main {
             e.printStackTrace();
         }
         long endTime = System.nanoTime();
-        double finishTime = (endTime-startTime)/1000000;
+        double finishTime = (endTime-startTime)/1000000.0;
         System.out.println(finishTime);
     }
-    public static Future<Integer> ovipar(int initialSum, int target, int counter){
+
+    public static Future<Integer> oviparCounter(int initialSum, int target, int counter){
         double temp1 = Math.floor(Math.sqrt(initialSum));
         double temp2 = (initialSum*initialSum);
         double temp3 = (initialSum*3) + 1;
         int rasio = (int)Math.floor(((temp2)+(temp3))/temp1);
+        Future<Integer> finalCounter = ovipar(initialSum, target, counter, rasio);
+        return  pool.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return finalCounter.get();
+            }
+        });
+    }
+    public static Future<Integer> ovipar(int initialSum, int target, int counter, int rasio){
         int result = initialSum * rasio;
         if(result <= target){
             counter += 1;
-            return ovipar(result, target, counter);
+            return ovipar(result, target, counter, rasio);
         }
-        int finalCouner = counter;
+        int finalCounter = counter;
         return  pool.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
-                return finalCouner;
+                return finalCounter;
             }
         });
     }
-    public static Future<Integer> vivipar(int initialSum, int target, int counter){
+    public static Future<Integer> viviparCounter(int initialSum, int target, int counter){
         double temp1 = Math.floor(Math.sqrt((initialSum*initialSum)+1));
         int rasio = (int)Math.floor((initialSum+temp1)/initialSum);
+        Future<Integer> finalCounter = vivipar(initialSum, target, counter, rasio);
+        return  pool.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return finalCounter.get();
+            }
+        });
+    }
+    public static Future<Integer> vivipar(int initialSum, int target, int counter, int rasio){
         int result = rasio * initialSum;
         if(result <= target){
             counter +=1;
-            return vivipar(result, target, counter);
+            return vivipar(result, target, counter, rasio);
         }
-        int finalCouner = counter;
+        int finalCounter = counter;
         return  pool.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
-                return finalCouner;
+                return finalCounter;
             }
         });
     }
-
-    public static Future<Integer> ovivipar(int initialSum, int target, int counter){
+    public static Future<Integer> oviviparCounter(int initialSum, int target, int counter){
         double temp1 = 2*(((initialSum*initialSum)+(3*initialSum)+1)/(Math.sqrt(initialSum)));
         double temp2 = 5*((initialSum+(Math.sqrt((initialSum)+((initialSum*initialSum)+1))))/initialSum);
         int rasio = (int)Math.floor(temp1+temp2);
-        int result = rasio * initialSum;
-        if(result <= target){
-            counter +=1;
-            return ovivipar(result, target, counter);
-        }
-        int finalCouner = counter;
+        Future<Integer> finalCounter = ovivipar(initialSum, target, counter, rasio);
         return  pool.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
-                return finalCouner;
+                return finalCounter.get();
+            }
+        });
+
+    }
+    public static Future<Integer> ovivipar(int initialSum, int target, int counter, int rasio){
+        int result = rasio * initialSum;
+        if(result <= target){
+            counter +=1;
+            return ovivipar(result, target, counter, rasio);
+        }
+        int finalCounter = counter;
+        return  pool.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return finalCounter;
             }
         });
     }
