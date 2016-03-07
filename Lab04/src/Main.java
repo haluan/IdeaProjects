c668aac0808da23133f85e9a24020cdb4c692961
Haluan <haluan@icehousecorp.com>
2016-03-07 12:42:30 +0700

    add future thread

--- Lab04/src/Main.java
+++ Lab04/src/Main.java
@@ -1,24 +1,50 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Callable;
 
 /**
  * Created by haluan on 3/7/16.
  */
 public class Main {
+    final static ExecutorService pool = Executors.newFixedThreadPool(10);
     public static void main(String[] args){
         long startTime = System.nanoTime();
         final int SIKLUS = 1;
         int initAyam = 9;
         int targetAyam = 900;
-        System.out.println(targetAyam + " ayam dalam "+ovipar(initAyam, targetAyam, SIKLUS) + " siklus");
+        Future<Integer> result = ovipar(initAyam, targetAyam, SIKLUS);
+        try {
+            System.out.println(targetAyam + " ayam dalam "+ result.get() + " siklus");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         int initKucing = 23;
         int targetKucing = 830;
-
-        System.out.println(targetKucing + " kucing dalam "+vivipar(initKucing, targetKucing, SIKLUS)+" siklus");
-        System.out.println(ovivipar(4, 900, SIKLUS));
+        result = vivipar(initKucing, targetKucing, SIKLUS);
+        try {
+            System.out.println(targetKucing + " kucing dalam "+ result.get() +" siklus");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        result = ovivipar(4, 900, SIKLUS);
+        try {
+            System.out.println(result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         long endTime = System.nanoTime();
-        long finishTime = (endTime-startTime)/1000000;
+        double finishTime = (endTime-startTime)/1000000;
         System.out.println(finishTime);
     }
-    public static int ovipar(int initialSum, int target, int counter){
+    public static Future<Integer> ovipar(int initialSum, int target, int counter){
         double temp1 = Math.floor(Math.sqrt(initialSum));
         double temp2 = (initialSum*initialSum);
         double temp3 = (initialSum*3) + 1;
@@ -28,9 +54,15 @@ public class Main {
             counter += 1;
             return ovipar(result, target, counter);
         }
-        return  counter;
+        int finalCouner = counter;
+        return  pool.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return finalCouner;
+            }
+        });
     }
-    public static int vivipar(int initialSum, int target, int counter){
+    public static Future<Integer> vivipar(int initialSum, int target, int counter){
         double temp1 = Math.floor(Math.sqrt((initialSum*initialSum)+1));
         int rasio = (int)Math.floor((initialSum+temp1)/initialSum);
         int result = rasio * initialSum;
@@ -38,10 +70,16 @@ public class Main {
             counter +=1;
             return vivipar(result, target, counter);
         }
-        return counter;
+        int finalCouner = counter;
+        return  pool.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return finalCouner;
+            }
+        });
     }
 
-    public static int ovivipar(int initialSum, int target, int counter){
+    public static Future<Integer> ovivipar(int initialSum, int target, int counter){
         double temp1 = 2*(((initialSum*initialSum)+(3*initialSum)+1)/(Math.sqrt(initialSum)));
         double temp2 = 5*((initialSum+(Math.sqrt((initialSum)+((initialSum*initialSum)+1))))/initialSum);
         int rasio = (int)Math.floor(temp1+temp2);
@@ -50,6 +88,12 @@ public class Main {
             counter +=1;
             return ovivipar(result, target, counter);
         }
-        return counter;
+        int finalCouner = counter;
+        return  pool.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return finalCouner;
+            }
+        });
     }
 }
